4551000ae584b8bc8d235e0e40833d804bf936ce
Hugo <hugo@Hugo-M1-Max-16.local>
2024-08-06 22:42:01 +0900

    [feature] baby dog main page style, feature modified with mockup

--- app/(main)/main/components/baby-dog-point-detail.tsx
+++ app/(main)/main/components/baby-dog-point-detail.tsx
@@ -100,17 +100,19 @@ export default function BabyDogPointDetail() {
   ];
 
   return (
-    <section className="flex flex-col items-center rounded-t-[50px] bg-[#FFF8F2] mt-[80px]">
+    <section className="flex flex-col items-center rounded-t-[50px] bg-[#FFF8F2] mt-[80px] pb-[90px]">
       <h2 className="text-4xl font-semibold mt-[44px]">Task</h2>
-      <div className="flex flex-col gap-2 mt-10">
-        <Table>
+      <div className="flex flex-col gap-2 mt-10 w-full max-w-[700px] px-[23px]">
+        <Table className="">
           <TableBody>
             {tasks.map((task) => (
               <TableRow key={task.mission}>
-                <TableCell className="px-0">{task.headerIcon()}</TableCell>
+                <TableCell className="px-0 w-[30px]">
+                  {task.headerIcon()}
+                </TableCell>
                 <TableCell className="px-2 gap-[3px]">
                   <div className="flex flex-col">
-                    <span className="text-muted-foreground font-medium">
+                    <span className="text-muted-foreground font-medium text-[16px] leading-6">
                       {task.mission}
                     </span>
                     <span className="text-[18px] leading-6 font-semibold">
@@ -118,7 +120,7 @@ export default function BabyDogPointDetail() {
                     </span>
                   </div>
                 </TableCell>
-                <TableCell className="px-0">
+                <TableCell className="px-0 flex justify-end">
                   {task.buttonType === "start" ? (
                     <Button variant={"gray"} className="w-20">
                       Start
@@ -135,32 +137,19 @@ export default function BabyDogPointDetail() {
         </Table>
       </div>
       <h2 className="text-4xl font-semibold mt-[44px]">Your rewards</h2>
-      <div className="flex flex-col gap-2 mt-10">
+      <div className="flex flex-col gap-2 mt-10 w-full max-w-[700px] px-[23px]">
         <Table className="w-full">
           <TableBody>
-            {tasks.map((task) => (
-              <TableRow key={task.mission}>
-                <TableCell className="px-0">{task.headerIcon()}</TableCell>
-                <TableCell className="px-2 gap-[3px]">
-                  <div className="flex flex-col">
-                    <span className="text-muted-foreground font-medium">
-                      {task.mission}
-                    </span>
-                    <span className="text-[18px] leading-6 font-semibold">
-                      + {task.point} BABY DOGS
-                    </span>
-                  </div>
+            {rewards.map((reward) => (
+              <TableRow key={reward.mission}>
+                <TableCell className="px-0 w-[30px]">
+                  {reward.headerIcon()}
                 </TableCell>
-                <TableCell className="px-0">
-                  {task.buttonType === "start" ? (
-                    <Button variant={"gray"} className="w-20">
-                      Start
-                    </Button>
-                  ) : (
-                    <Button variant={"orange"} className="w-20">
-                      Check
-                    </Button>
-                  )}
+                <TableCell className="px-2 gap-[3px] text-muted-foreground text-[16px] leading-6">
+                  {reward.mission}
+                </TableCell>
+                <TableCell className="px-0 text-[18px] leading-6 font-semibold text-end">
+                  + {reward.point} BABY DOG
                 </TableCell>
               </TableRow>
             ))}
