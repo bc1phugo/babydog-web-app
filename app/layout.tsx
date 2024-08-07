c1daf7b62ac4c0e87fac2e59c40aa650db9eb86b
Hugo <hugo@Hugo-M1-Max-16.local>
2024-08-08 00:34:56 +0900

    [fix] max-h-dvh deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="max-h-dvh">
+    <html lang="en">
       <head>
         {/* <Script
           src="https://telegram.org/js/telegram-web-app.js"
