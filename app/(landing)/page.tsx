d1bc0ffa4d040e9a56caddf637a1108439a962bb
Hugo <hugo@Hugo-M1-Max-16.local>
2024-08-18 02:20:09 +0900

    [fix] route.ts fixed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -22,8 +22,7 @@ export default function LandingPage() {
   }, []);
 
   useEffect(() => {
-    if (!user) return;
-    alert(user.id);
+    // alert(user.id);
 
     try {
       fetch(`/api/user`, {
@@ -32,14 +31,14 @@ export default function LandingPage() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          telegram_id: user.id,
-          username: user.username,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          language_code: user.language_code,
-          is_premium: user.is_premium,
-          photo_url: user.photo_url,
-          referral_code: referral,
+          telegram_id: 2213,
+          username: "testuser",
+          first_name: "Test",
+          last_name: "User",
+          language_code: "en",
+          is_premium: false,
+          photo_url: "http://example.com/photo.jpg",
+          referral_code: null,
         }),
       })
         .then((res) => {
---
+++ app/api/user/route.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+// import Cors from "cors"; // Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+// const cors = Cors({
+//   methods: ["POST", "GET", "HEAD"],
+// });
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+// function runMiddleware(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+//   fn: Function
+// ) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+
+//       return resolve(result);
+//     });
+//   });
+// }
+
+export async function POST(req: Request) {
+  const res = await req.json();
+  try {
+    const response = await fetch(`http://43.203.236.52:13000/api/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(res),
+    });
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
