3921e3c28a16c27ab01a9a13e4aeb87ba187cb35
Hugo <hugo@Hugo-M1-Max-16.local>
2024-08-19 01:20:19 +0900

    [fix] account score view height modified

--- app/(landing)/account-score/components/phase1.tsx
+++ app/(landing)/account-score/components/phase1.tsx
@@ -8,7 +8,7 @@ export default function Phase1({ telegramId }: Phase1Props) {
       <div className="mt-[10px] text-muted-foreground text-md tracking-tight">
         You have joined Telegram
       </div>
-      <div className="mt-[60px] mb-[30px]">
+      <div className="mt-[20px] mb-[10px]">
         <div className="text-[220px] leading-[180px] font-semibold">0</div>
         <div className="text-4xl font-semibold mt-[10px]">years ago</div>
         <div className="text-base mt-[59px] tracking-tight">
--- app/(landing)/account-score/components/phase2.tsx
+++ app/(landing)/account-score/components/phase2.tsx
@@ -12,10 +12,10 @@ export default function Phase2({ dogPoint }: Phase2Props) {
         <link rel="preload" href="/images/babydog-2.png" as="image" />
       </Head>
       <h1 className="text-4xl font-semibold mt-[20px]">You are amazing!</h1>
-      <div className="mt-[10px] text-muted-foreground tracking-tight">
+      <div className="mt-[5px] text-muted-foreground tracking-tight">
         Here is your DOGS reward
       </div>
-      <div className="mt-[60px] mb-[25px]">
+      <div className="mt-[20px] mb-[10px]">
         <Image
           priority
           src="/images/babydog-2.png"
@@ -25,7 +25,7 @@ export default function Phase2({ dogPoint }: Phase2Props) {
           className="m-auto"
         />
         <div className="text-4xl font-semibold mt-[14px]">{dogPoint ?? 0}</div>
-        <div className="text-base mt-[60px] tracking-tight">
+        <div className="text-base mt-[40px] tracking-tight">
           Thanks for your time on Telegram
         </div>
       </div>
