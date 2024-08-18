8001e95b88a64e74315804565ca04bf0c7e4530e
hugooh <hugo.oh.dev@gmail.com>
2024-08-19 08:42:13 +0900

    [fix] Revalidate userinfo After user created

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,19 +1,26 @@
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const res = await req.json();
+  const body = await req.json();
+  const telegramId = body.telegramId;
+  console.log("🚀 ~ POST ~ telegramId:", telegramId);
+
   try {
     const response = await fetch(`${process.env.API_URL}/api/user`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(res),
+      body: JSON.stringify(body),
       next: {
         revalidate: 60,
       },
     });
     const data = await response.json();
+
+    revalidateTag(`userInfo-${telegramId}`);
+
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
