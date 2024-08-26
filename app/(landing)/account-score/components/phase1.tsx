f3b1261e5e110745de2f26a224abefc06b4394bc
hugooh <hugo.oh.dev@gmail.com>
2024-08-26 10:38:40 +0900

    [feature] landing account score images set

--- app/(landing)/account-score/components/phase1.tsx
+++ app/(landing)/account-score/components/phase1.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { IUserInfo } from "@/hooks/useUserInfo";
-
+import Image from "next/image";
 interface Phase1Props {
   telegramId: number;
   userInfo: IUserInfo | undefined;
@@ -23,12 +23,20 @@ export default function Phase1({ telegramId, userInfo }: Phase1Props) {
   return (
     <>
       <h1 className="mt-[20px] text-4xl font-semibold">We are DOG Army!</h1>
+      <Image
+        priority
+        src="/images/image_rocket.png"
+        width={417}
+        height={284}
+        alt={"image-baby-dog"}
+        className="absolute left-1/2 -z-10 m-auto min-h-[284px] min-w-[417px] -translate-x-1/2"
+      />
       <div className="mt-[10px] text-md tracking-tight text-muted-foreground">
         You have joined Telegram
       </div>
       <div className="mb-[10px] mt-[20px]">
         <div className="text-[220px] font-semibold leading-[180px]">
-          {yearsSinceJoined}
+          {Number.isNaN(yearsSinceJoined) ? 0 : yearsSinceJoined}
         </div>
         <div className="mt-[10px] text-4xl font-semibold">years ago</div>
         <div className="mt-[59px] text-base tracking-tight">
--- app/(landing)/account-score/components/phase2.tsx
+++ app/(landing)/account-score/components/phase2.tsx
@@ -11,21 +11,21 @@ export default function Phase2({ dogPoint }: Phase2Props) {
       <Head>
         <link rel="preload" href="/images/babydog-2.png" as="image" />
       </Head>
+      <Image
+        priority
+        src="/images/babydog-2-confetti.png"
+        width={422}
+        height={444}
+        alt={"image-baby-dog"}
+        className="absolute left-1/2 top-[-63px] -z-10 m-auto min-h-[444px] min-w-[422px] -translate-x-1/2"
+      />
       <h1 className="mt-[20px] text-4xl font-semibold">You are amazing!</h1>
       <div className="mt-[5px] tracking-tight text-muted-foreground">
         Here is your BABY DOGS reward
       </div>
-      <div className="mb-[10px] mt-[20px]">
-        <Image
-          priority
-          src="/images/babydog-2.png"
-          width={249}
-          height={202}
-          alt={"image-baby-dog"}
-          className="m-auto"
-        />
+      <div className="mb-[10px] mt-[276px]">
         <div className="mt-[14px] text-4xl font-semibold">{dogPoint ?? 0}</div>
-        <div className="mt-[40px] text-base tracking-tight">
+        <div className="mt-[20px] text-base tracking-tight">
           Thanks for your time on Telegram
         </div>
       </div>
