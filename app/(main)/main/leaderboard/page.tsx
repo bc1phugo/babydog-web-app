e3b7f09c6e2ca80de62d331db95f29f5e7dce38c
Hugo <hugo@Hugo-M1-Max-16.local>
2024-08-29 22:14:22 +0900

    [feature] leaderboard Skeleton set

--- app/(main)/main/leaderboard/page.tsx
+++ app/(main)/main/leaderboard/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { useTelegram } from "@/app/providers/telegram-provider";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { buttonVariants } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import useUserRankingsQuery from "@/hooks/useUserRankings";
 import { cn, formatNumber } from "@/lib/utils";
@@ -8,11 +10,9 @@ import Image from "next/image";
 
 export default function LeaderboardPage() {
   const { data: userRankings } = useUserRankingsQuery({ customEnabled: true });
+  const { user } = useTelegram();
 
-  const userName =
-    userRankings?.userRank.username ??
-    userRankings?.userRank.first_name ??
-    "Mr.Unknown";
+  const userName = user?.username ?? user?.first_name ?? "Mr.Unknown";
 
   const getRankingCellContent = (targetRank: number) => {
     switch (targetRank) {
@@ -84,71 +84,122 @@ export default function LeaderboardPage() {
             Telegram <br />
             Wall of Fame
           </div>
-          <div
-            className={cn(
-              buttonVariants({ variant: "gray" }),
-              "mt-[44px] flex h-20 w-full justify-start pl-5 pr-[14px]",
-            )}
-          >
-            <Avatar className="mr-[10px]">
-              <AvatarFallback className="bg-purple-600 text-[18px] leading-4 tracking-tight text-background">
-                {getInitials(userName)}
-              </AvatarFallback>
-            </Avatar>
-            <div className="text-start">
-              <div className="text-[16px] font-medium leading-6 tracking-tight">
-                {userName}
+          {userRankings ? (
+            <div
+              className={cn(
+                buttonVariants({ variant: "gray" }),
+                "mt-[44px] flex h-20 w-full justify-start pl-5 pr-[14px]",
+              )}
+            >
+              <Avatar className="mr-[10px]">
+                <AvatarFallback className="bg-purple-600 text-[18px] leading-4 tracking-tight text-background">
+                  {getInitials(userName)}
+                </AvatarFallback>
+              </Avatar>
+              <div className="text-start">
+                <div className="text-[16px] font-medium leading-6 tracking-tight">
+                  {userName}
+                </div>
+                <div className="text-md font-semibold tracking-tight text-primary">
+                  {userRankings.userRank.baby_dog_points} BABY DOGS
+                </div>
               </div>
-              <div className="text-md font-semibold tracking-tight text-primary">
-                {userRankings.userRank.baby_dog_points} BABY DOGS
+              <div className="ml-auto text-md font-medium">
+                {getRankingCellContent(Number(userRankings.userRank.rank)) ??
+                  "-"}
               </div>
             </div>
-            <div className="ml-auto text-md font-medium">
-              {getRankingCellContent(Number(userRankings.userRank.rank)) ?? "-"}
-            </div>
-          </div>
+          ) : (
+            <Skeleton
+              className={cn(
+                "mt-[44px] flex h-20 w-full animate-pulse justify-start rounded-full pl-5 pr-[14px]",
+              )}
+            />
+          )}
         </section>
       </div>
       <section className="mt-[50px] flex flex-col items-center pb-[140px]">
-        <h2 className="text-4xl font-semibold">
-          {formatNumber(userRankings.userCount) ?? "-"} holders
-        </h2>
-        <div className="mt-8 flex w-full max-w-[700px] flex-col gap-2 px-[23px]">
-          <Table className="">
-            <TableBody>
-              {userRankings.top20.map((ranker) => {
-                const userName =
-                  ranker.username ?? ranker.first_name ?? "Mr.Unkown";
-                return (
-                  <TableRow key={`${userName}-${ranker.rank}`}>
+        {userRankings ? (
+          <>
+            <h2 className="text-4xl font-semibold">
+              {formatNumber(userRankings.userCount) ?? "-"} holders
+            </h2>
+            <div className="mt-8 flex w-full max-w-[700px] flex-col gap-2 px-[23px]">
+              <Table className="">
+                <TableBody>
+                  {userRankings.top20.map((ranker) => {
+                    const userName =
+                      ranker.username ?? ranker.first_name ?? "Mr.Unkown";
+                    return (
+                      <TableRow key={`${userName}-${ranker.rank}`}>
+                        <TableCell className="w-[40px] px-0">
+                          <Avatar>
+                            <AvatarFallback className="bg-red-600 text-[18px] leading-4 tracking-tight text-background">
+                              {getInitials(userName)}
+                            </AvatarFallback>
+                          </Avatar>
+                        </TableCell>
+                        <TableCell className="gap-[3px] pl-2 pr-0 tracking-tight">
+                          <div className="flex flex-col">
+                            <span className="text-[16px] font-medium leading-6 tracking-tight text-muted-foreground">
+                              {ranker.username ??
+                                ranker.first_name ??
+                                "Mr.Unkown"}
+                            </span>
+                            <span className="text-[18px] font-semibold leading-6 tracking-tight">
+                              + {ranker.baby_dog_points} BABY DOGS
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell className="justify-end px-0 text-[24px] leading-6">
+                          <span className="flex justify-end font-semibold">
+                            {getRankingCellContent(Number(ranker.rank))}
+                          </span>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </div>
+          </>
+        ) : (
+          <>
+            <Skeleton className="h-[46px] w-[200px] animate-pulse rounded-full"></Skeleton>
+            <div className="mt-8 flex w-full max-w-[700px] flex-col gap-2 px-[23px]">
+              <Table>
+                <TableBody>
+                  <TableRow className="border-b-muted">
                     <TableCell className="w-[40px] px-0">
                       <Avatar>
-                        <AvatarFallback className="bg-red-600 text-[18px] leading-4 tracking-tight text-background">
-                          {getInitials(userName)}
-                        </AvatarFallback>
+                        <AvatarFallback className="animate-pulse bg-slate-200 text-[18px] leading-4 tracking-tight text-background" />
                       </Avatar>
                     </TableCell>
                     <TableCell className="gap-[3px] pl-2 pr-0 tracking-tight">
-                      <div className="flex flex-col">
-                        <span className="text-[16px] font-medium leading-6 tracking-tight text-muted-foreground">
-                          {ranker.username ?? ranker.first_name ?? "Mr.Unkown"}
-                        </span>
-                        <span className="text-[18px] font-semibold leading-6 tracking-tight">
-                          + {ranker.baby_dog_points} BABY DOGS
-                        </span>
+                      <div className="flex flex-col gap-1">
+                        <Skeleton className="h-6 w-full animate-pulse text-[16px] font-medium leading-6 tracking-tight text-muted-foreground" />
+                        <Skeleton className="h-6 w-full animate-pulse text-[18px] font-semibold leading-6 tracking-tight"></Skeleton>
                       </div>
                     </TableCell>
-                    <TableCell className="justify-end px-0 text-[24px] leading-6">
-                      <span className="flex justify-end font-semibold">
-                        {getRankingCellContent(Number(ranker.rank))}
-                      </span>
+                  </TableRow>
+                  <TableRow className="border-b-muted">
+                    <TableCell className="w-[40px] px-0">
+                      <Avatar>
+                        <AvatarFallback className="animate-pulse bg-slate-200 text-[18px] leading-4 tracking-tight text-background" />
+                      </Avatar>
+                    </TableCell>
+                    <TableCell className="gap-[3px] pl-2 pr-0 tracking-tight">
+                      <div className="flex flex-col gap-1">
+                        <Skeleton className="h-6 w-full animate-pulse text-[16px] font-medium leading-6 tracking-tight text-muted-foreground" />
+                        <Skeleton className="h-6 w-full animate-pulse text-[18px] font-semibold leading-6 tracking-tight"></Skeleton>
+                      </div>
                     </TableCell>
                   </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
+                </TableBody>
+              </Table>
+            </div>
+          </>
+        )}
       </section>
     </>
   );
